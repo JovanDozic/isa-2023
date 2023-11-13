a1981086fabce0795bdb84ba483cf560d356ca9a
Jovan Dozic <jovandozic@gmail.com>
2023-11-13 09:35:57 +0100

    Database setup finished.

---
+++ MedEquipCentral-Backend/MedEquipCentral/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace MedEquipCentral
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
--- MedEquipCentral-Backend/MedEquipCentral/Data/DataContext.cs
+++ MedEquipCentral-Backend/MedEquipCentral/Data/DataContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MedEquipCentral;
 
 namespace MedEquipCentral.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<MedEquipCentral.Company>? Company { get; set; }
     }
 }
