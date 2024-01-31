07f8be7538df2a27ca33ff1f68cbcc67f3f143e3
Luka <luka.todorovic2001@gmail.com>
2024-01-31 18:36:31 +0100

    --

---
+++ MedEquipCentral-Backend/MedEquipCentral.DA/Migrations/20240131173450_ReservedEquip.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedEquipCentral.DA.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReservedEquip : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Reserved",
+                table: "Equipment",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reserved",
+                table: "Equipment");
+        }
+    }
+}
